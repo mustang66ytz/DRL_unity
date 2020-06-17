080b5b231d5521a6f8c48c882746c291b998a9e9
Taozheng Yang <johnyang66ytz@gmail.com>
2020-06-17 19:43:11 +0800

    added moving obstacle mobile robot training environments

---
+++ Assets/MoveObstacle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+	Rigidbody rBody;
+	public float speed = 0.3f;
+	public float z_coordinate = 2f;
+	Vector3 dirToGo = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+	transform.localPosition = new Vector3(0f, 2.5f, z_coordinate);
+    }
+	
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+	if(transform.position.x>5){
+		// move left
+		dirToGo = transform.forward*1f*speed;
+		rBody.AddForce(dirToGo, ForceMode.VelocityChange);
+		
+	}
+	if(dirToGo == Vector3.zero){
+		dirToGo = transform.forward*-1f*speed;
+	}        
+	else if(transform.position.x<-30){
+		// move right
+		dirToGo = transform.forward*-1f*speed;
+		rBody.AddForce(dirToGo, ForceMode.VelocityChange);
+	}
+	else{
+		rBody.transform.position += dirToGo;
+	}
+    }
+}
